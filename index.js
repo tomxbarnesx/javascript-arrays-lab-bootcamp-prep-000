aa9c1d6f882aca925d690eae9dda97846734e8d0
Thomas Barnes <barnes.thomas.c@gmail.com>
2018-05-22 19:30:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const app = "I don't do much."
+var kittenArray = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name) {
-  
+  kittenArray.push(name);
+}
+
+function destructivelyPrependKitten(name) {
+  kittenArray.unshift(name);
 }
